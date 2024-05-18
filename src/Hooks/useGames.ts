2ec05360ea0e4c358be810b76a549fdc48afd3d3
Jaro <jaroslavfilo95@gmail.com>
2024-05-18 18:11:46 +0200

    shwo loading skeletons

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -25,20 +25,28 @@ const useGames = () => {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
   
     useEffect(() => {
       const controller = new AbortController();
 
+
+    setLoading(true);
     APIClient.get<FetchGamesResponse>("/games", { signal: controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message)
+        setLoading(false);
+      });
 
       return() => controller.abort();
 }, []); // if you do not include dependency, you will constantly sent request to backend, BIG NO NO
   
-return { games, error };
+return { games, error, isLoading };
 
 }
 
